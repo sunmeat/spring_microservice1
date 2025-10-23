2ce684c3181bc6b39fb6d6b177184b5a54c07c6f
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-10-23 14:49:41 +0300

    Refactor WebConfig class location to config package
    Moved WebConfig class to the config package without significant changes.

--- SpringMVC/src/main/java/com/sunmeat/contactlist/config/WebConfig.java
+++ SpringMVC/src/main/java/com/sunmeat/contactlist/config/WebConfig.java
@@ -1,12 +1,11 @@
-package com.sunmeat.contactlist.config;
+package com.sunmeat.contactlist.config; // файл без суттєвих змін, просто переїхав в пакет config
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration // конфігурація для обслуговування статичних ресурсів
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
