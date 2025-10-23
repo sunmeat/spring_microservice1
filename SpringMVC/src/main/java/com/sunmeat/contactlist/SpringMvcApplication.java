b74cd109df6901959bb1d6f477ea0731d62656bb
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-10-23 14:59:54 +0300

    Implement BrowserLauncher to open URL on startup
    Added BrowserLauncher component to open browser on application start.

--- SpringMVC/src/main/java/com/sunmeat/contactlist/SpringMvcApplication.java
+++ SpringMVC/src/main/java/com/sunmeat/contactlist/SpringMvcApplication.java
@@ -5,9 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMvcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
+}
 
+@Component
+class BrowserLauncher {
+	@EventListener(ApplicationReadyEvent.class)
+	public void launchBrowser() {
+		System.setProperty("java.awt.headless", "false"); 
+		var desktop = Desktop.getDesktop();
+		try {
+			desktop.browse(new URI("http://localhost:8080/profiles"));
+		} catch (Exception e) {
+			
+		}
+	}
 }
